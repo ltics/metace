c32880638b3400f48af5e6eec5791ddc91e19f2f
locl <zjhmale@gmail.com>
2015-08-08 00:04:19 +0800

    finish driver loop

--- src/metace/core.clj
+++ src/metace/core.clj
@@ -1,7 +1,50 @@
 (ns metace.core
-  (:gen-class))
+  (:require [metace.meta-env :refer :all]
+            [metace.meta-eval :refer :all]
+            [metace.meta-apply :refer :all]
+            [metace.eval-apply :refer :all]))
+
+(defn setup-environment
+  []
+  (let [initial-env (extend-environment (primitive-procedure-names)
+                                        (primitive-procedure-objects)
+                                        the-empty-environment)]
+    (define-variable! 'true true initial-env)
+    (define-variable! 'false false initial-env)
+    initial-env))
+
+(def the-global-environment (setup-environment))
+
+(def input-prompt ";;; M-Eval input:")
+(def output-prompt ";;; M-Eval value:")
+
+(defn prompt-for-input
+  [string]
+  (do (prn) (prn) (prn string) (prn)))
+
+(defn announce-output
+  [string]
+  (do (prn) (prn string) (prn)))
+
+(defn user-print
+  [object]
+  (if (compound-procedure? object)
+    (prn (list 'compound-procedure
+               (procedure-parameters object)
+               (procedure-body object)
+               '<procedure-env>))
+    (prn object)))
+
+(defn driver-loop
+  []
+  (do
+    (prompt-for-input input-prompt)
+    (let [input (read)]
+      (let [output (metaeval input the-global-environment)]
+        (announce-output output-prompt)
+        (user-print output)))
+    (driver-loop)))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (driver-loop))
