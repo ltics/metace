0999ddc24ddf068275839f56d2c170705b89175c
zjhmale <zjhmale@gmail.com>
2015-10-05 17:46:08 +0800

    update project metainfo

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject metace "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "meta circular evaluator in clojure"
+  :url "https://github.com/zjhmale/metace"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
